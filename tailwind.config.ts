6973577df6711ed7e5b77415cf25a57911fa1752
Swati Jain <jain.swati71@gmail.com>
2024-06-19 18:35:09 +0200

    Added tailwind config file and removed basic styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,9 @@
-/** @type {import('tailwindcss').Config} */
+import type { Config } from "tailwindcss";
+
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+} satisfies Config;
