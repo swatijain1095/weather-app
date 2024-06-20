6dbcacee7b99f58770b6896ebea096fb68486ef6
Swati Jain <jain.swati71@gmail.com>
2024-06-20 21:24:22 +0200

    added images required and also added search bar in app.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
+import { useState } from "react";
 import "./App.css";
+import search from "./assets/icons/search.svg";
 
 function App() {
-  return <div className="text-xl font-bold text-center">hello world</div>;
+  const [input, setInput] = useState("");
+  return (
+    <div className="w-full h-screen text-white px-8">
+      <nav className="w-full p-3 flex justify-between items-center">
+        <h1 className="font-bold tracking-wide text-3xl">Weather App</h1>
+        <div className="bg-white w-[15rem] overflow-hidden shadow-2xl rounded flex items-center p-2 g-2">
+          <img src={search} alt="search" className="w-[1.5rem] h-[1.5rem]" />
+          <input
+            type="text"
+            className="focus:outline-none w-full text-[#212121] text-lg"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            onKeyUp={(e) => {
+              if (e.key === "Enter") {
+                //submit form
+              }
+            }}
+          />
+        </div>
+      </nav>
+    </div>
+  );
 }
 
 export default App;
